042fe2e6590366afb218c99318fdfb7c2fb8ff05
Andrea Vrljic <AVrljic@dspace.hr>
2022-02-03 15:12:02 +0100

    Change loading color

--- LoadingWindow.Designer.cs
+++ LoadingWindow.Designer.cs
@@ -37,8 +37,8 @@ namespace WordsCounterList
             this.processing.AccessibleName = "";
             this.processing.AutoSize = true;
             this.processing.Font = new System.Drawing.Font("MV Boli", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.processing.ForeColor = System.Drawing.Color.FloralWhite;
-            this.processing.Location = new System.Drawing.Point(77, 34);
+            this.processing.ForeColor = System.Drawing.Color.DarkGreen;
+            this.processing.Location = new System.Drawing.Point(77, 33);
             this.processing.Name = "processing";
             this.processing.Size = new System.Drawing.Size(133, 31);
             this.processing.TabIndex = 0;
@@ -48,7 +48,7 @@ namespace WordsCounterList
             // LoadingWindow
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackColor = System.Drawing.Color.Gold;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(282, 103);
             this.ControlBox = false;
